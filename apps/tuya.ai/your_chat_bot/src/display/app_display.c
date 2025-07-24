00e28f119bada3b24a73354b76bb577ac7823c15
maidang-xing <maidang.xing@tuya.com>
2025-07-24 13:55:30 +0800

    Surport board DNESP32S3_BOX2_WIFI (#281)

--- apps/tuya.ai/your_chat_bot/src/display/app_display.c
+++ apps/tuya.ai/your_chat_bot/src/display/app_display.c
@@ -1,5 +1,6 @@
 /**
  * @file app_display.c
+ * @author Tuya Inc.
  * @brief Handle display initialization and message processing
  *
  * This source file provides the implementation for initializing the display system,
@@ -76,7 +77,7 @@ static UI_EMOJI_LIST_T sg_awesome_emo_list[] = {
 #endif
 
 static __attribute__((unused)) UI_EMOJI_LIST_T sg_emo_list[EMO_ICON_MAX_NUM] = {
-    {"NEUTRAL", "😶"},  {"SAD", "😔"},         {"ANGRY", "😠"},    {"SURPRISE", "😯"},
+    {"NEUTRAL", "😶"},  {"SAD", "😔"},      {"ANGRY", "😠"}, {"SURPRISE", "😯"},
     {"CONFUSED", "😏"}, {"THINKING", "🤔"}, {"HAPPY", "🙂"},
 };
 
@@ -95,7 +96,8 @@ static OPERATE_RET __get_ui_font(UI_FONT_T *ui_font)
     }
 
 #if (defined(BOARD_CHOICE_TUYA_T5AI_BOARD) || defined(BOARD_CHOICE_TUYA_T5AI_EVB) ||                                   \
-     defined(BOARD_CHOICE_T5AI_MOJI_1_28) || defined(BOARD_CHOICE_T5AI_MINI) || defined(BOARD_CHOICE_DNESP32S3_BOX))
+     defined(BOARD_CHOICE_T5AI_MOJI_1_28) || defined(BOARD_CHOICE_T5AI_MINI) || defined(BOARD_CHOICE_DNESP32S3_BOX) || \
+     defined(BOARD_CHOICE_DNESP32S3_BOX2_WIFI))
 #if defined(ENABLE_GUI_WECHAT)
     ui_font->text = (lv_font_t *)&font_puhui_18_2;
     ui_font->icon = (lv_font_t *)&font_awesome_16_4;
