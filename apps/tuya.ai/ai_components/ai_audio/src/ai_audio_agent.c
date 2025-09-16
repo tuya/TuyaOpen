64f618299c6dd2a3c67d95dc25583126e04c9944
YangJie <shiliu.yang@tuya.com>
2025-09-16 10:53:33 +0800

    feat: remove ai_audio_debug

--- apps/tuya.ai/ai_components/ai_audio/src/ai_audio_agent.c
+++ apps/tuya.ai/ai_components/ai_audio/src/ai_audio_agent.c
@@ -25,7 +25,6 @@
 #endif
 
 #include "ai_audio.h"
-#include "ai_audio_debug.h"
 
 /***********************************************************
 ************************macro define************************
@@ -461,10 +460,6 @@ static OPERATE_RET __ai_agent_init(void *data)
 {
     PR_DEBUG("%s...", __func__);
 
-#if defined(AI_AUDIO_DEBUG) && (AI_AUDIO_DEBUG == 1)
-    ai_audio_debug_init();
-#endif
-
 #if defined(ENABLE_AI_MONITOR) && (ENABLE_AI_MONITOR == 1)
     OPERATE_RET rt = OPRT_OK;
     ai_monitor_config_t monitor_cfg = AI_MONITOR_CFG_DEFAULT;
@@ -507,10 +502,6 @@ OPERATE_RET ai_audio_agent_init(AI_AGENT_CBS_T *cbs)
 OPERATE_RET ai_audio_agent_upload_start(uint8_t enable_vad)
 {
     OPERATE_RET rt = OPRT_OK;
-
-#if defined(AI_AUDIO_DEBUG) && (AI_AUDIO_DEBUG == 1)
-    ai_audio_debug_start();
-#endif
 
     PR_DEBUG("tuya ai upload start...");
 
@@ -556,10 +547,6 @@ OPERATE_RET ai_audio_agent_upload_start(uint8_t enable_vad)
 OPERATE_RET ai_audio_agent_upload_data(uint8_t *data, uint32_t len)
 {
     OPERATE_RET rt = OPRT_OK;
-
-#if defined(AI_AUDIO_DEBUG) && (AI_AUDIO_DEBUG == 1)
-    ai_audio_debug_data((char *)data, len);
-#endif
 
     // send data use tuya_ai_send_biz_pkt, pcm
     AI_BIZ_ATTR_INFO_T attr = {
@@ -612,10 +599,6 @@ OPERATE_RET ai_audio_agent_upload_stop(void)
     OPERATE_RET rt = OPRT_OK;
 
     PR_DEBUG("tuya ai upload stop...");
-
-#if defined(AI_AUDIO_DEBUG) && (AI_AUDIO_DEBUG == 1)
-    ai_audio_debug_stop();
-#endif
 
     TUYA_CALL_ERR_RETURN(ai_audio_agent_upload_data(NULL, 0));
 
