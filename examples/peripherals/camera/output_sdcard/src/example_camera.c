147eb4e564ba268371e652b9dd904846d75f9418
zhouss@tuya.com <zhouss@tuya.com>
2025-10-13 11:38:40 +0800

    feat:add camera output sdcard example

--- examples/peripherals/camera/output_sdcard/src/example_camera.c
+++ examples/peripherals/camera/output_sdcard/src/example_camera.c
@@ -1,19 +1,21 @@
 /**
- * @file example_dvp.c
+ * @file example_camera.c
  * @version 0.1
  * @copyright Copyright (c) 2021-2025 Tuya Inc. All Rights Reserved.
  */
 
 #include "tuya_cloud_types.h"
+
 #include "tal_api.h"
 #include "tkl_output.h"
-
-#include "tkl_video_in.h"
-#include "tkl_video_enc.h"
-
 #include "tkl_fs.h"
+
+#include "board_com_api.h"
+
+
+#include "tdl_camera_manage.h"
 /***********************************************************
-************************macro define************************
+*************************micro define***********************
 ***********************************************************/
 #define CAPTURED_FRAME_PATH_LEN 128
 #define VIDEO_FILE_DIR          "/sdcard"
@@ -22,27 +24,17 @@
 ***********************typedef define***********************
 ***********************************************************/
 
-
 /***********************************************************
 ***********************variable define**********************
 ***********************************************************/
-static char captured_frame_path[CAPTURED_FRAME_PATH_LEN] = {0};
+static TDL_CAMERA_HANDLE_T sg_tdl_camera_hdl = NULL;
+static bool sg_is_sdcard_init = false;
+static char sg_captured_frame_path[CAPTURED_FRAME_PATH_LEN] = {0};
 
 /***********************************************************
 ***********************function define**********************
 ***********************************************************/
-static OPERATE_RET  __fs_sd_init(void)
-{
-    OPERATE_RET rt = OPRT_OK;
-
-    TUYA_CALL_ERR_RETURN(tkl_fs_mount(VIDEO_FILE_DIR, DEV_SDCARD));
-    
-    PR_NOTICE("mount sd card success ");
-
-    return rt;
-} 
-
-static OPERATE_RET __fs_sd_save_file(char *file_path, void *data, uint32_t data_len)
+static OPERATE_RET __sdcard_save_file(char *file_path, void *data, uint32_t data_len)
 {
     OPERATE_RET rt = OPRT_OK;
 
@@ -83,19 +75,24 @@ static OPERATE_RET __fs_sd_save_file(char *file_path, void *data, uint32_t data_
     return rt;
 }
 
-static int __dvp_h264_cb(TKL_VENC_FRAME_T *pframe)
+OPERATE_RET __get_camera_h264_frame_cb(TDL_CAMERA_HANDLE_T hdl,  TDL_CAMERA_FRAME_T *frame)
 {
+
     OPERATE_RET rt = OPRT_OK;
 
-    if (NULL == pframe || NULL == pframe->pbuf || 0 == pframe->buf_size) {
+    if (NULL == hdl || NULL == frame->data || 0 == frame->data_len) {
         return 0;
     }
 
-    if (pframe->frametype != TKL_VIDEO_I_FRAME) {
+    if (0 == frame->is_i_frame) {
         return 0;
     }
 
-    memset(captured_frame_path, 0, CAPTURED_FRAME_PATH_LEN);
+    if(false == sg_is_sdcard_init){
+        return 0;
+    }
+
+    memset(sg_captured_frame_path, 0, CAPTURED_FRAME_PATH_LEN);
 
     POSIX_TM_S local_tm;
     memset((uint8_t *)&local_tm, 0x00, SIZEOF(POSIX_TM_S));
@@ -104,61 +101,69 @@ static int __dvp_h264_cb(TKL_VENC_FRAME_T *pframe)
         PR_ERR("Failed to get local time, rt = %d", rt);
         return 0;
     }
-    snprintf(captured_frame_path, CAPTURED_FRAME_PATH_LEN, "%s/%02d_%02d_%02d",\
+    snprintf(sg_captured_frame_path, CAPTURED_FRAME_PATH_LEN, "%s/%02d_%02d_%02d",\
             VIDEO_FILE_DIR, local_tm.tm_hour, local_tm.tm_min, local_tm.tm_sec);
-    PR_NOTICE("File name: %s", captured_frame_path);
+    PR_NOTICE("File name: %s", sg_captured_frame_path);
 
     // Save the frame to a file
-    rt = __fs_sd_save_file(captured_frame_path, pframe->pbuf, pframe->buf_size);
+    rt = __sdcard_save_file(sg_captured_frame_path, frame->data, frame->data_len);
     if (rt != OPRT_OK) {
         PR_ERR("Failed to save file, rt = %d", rt);
         return 0;
     }
-    PR_DEBUG("File saved successfully: %s", captured_frame_path);
+    PR_DEBUG("File saved successfully: %s", sg_captured_frame_path);
 
-    return 0;
+    return 0;   
+
+
 }
 
-static OPERATE_RET __example_dvp_init(void)
+static OPERATE_RET __sdcard_init(void)
 {
     OPERATE_RET rt = OPRT_OK;
-    TKL_VI_CONFIG_T vi_config;
-    TKL_VI_EXT_CONFIG_T ext_conf;
 
-    ext_conf.type = TKL_VI_EXT_CONF_CAMERA;
-    ext_conf.camera.camera_type = TKL_VI_CAMERA_TYPE_DVP;
-    ext_conf.camera.fmt = TKL_CODEC_VIDEO_H264;
+    TUYA_CALL_ERR_RETURN(tkl_fs_mount(VIDEO_FILE_DIR, DEV_SDCARD));
 
-    ext_conf.camera.power_pin = TUYA_GPIO_NUM_51;
-    ext_conf.camera.active_level = TUYA_GPIO_LEVEL_HIGH;
-    ext_conf.camera.i2c.clk = TUYA_GPIO_NUM_13;
-    ext_conf.camera.i2c.sda = TUYA_GPIO_NUM_15;
-    ext_conf.camera.fps = 15;
-    ext_conf.camera.height = 480;
-    ext_conf.camera.width = 480;
+    sg_is_sdcard_init = true;
+    
+    PR_NOTICE("mount sd card success ");
 
-    vi_config.isp.width = 480;
-    vi_config.isp.height = 320;
-    vi_config.isp.fps = 15;
 
-    vi_config.pdata = &ext_conf;
-    vi_config.disp_cb = NULL;
-
-    TUYA_CALL_ERR_RETURN(tkl_vi_init(&vi_config, 0));
-
-    TKL_VENC_CONFIG_T h264_config;
-    // DVP:0; UVC:1
-    h264_config.enable_h264_pipeline = 0; // dvp
-    h264_config.put_cb = __dvp_h264_cb;
-    PR_NOTICE("h264_config.put_cb:%p", h264_config.put_cb);
-    // TUYA_CALL_ERR_RETURN(tkl_venc_init(0, &h264_config, 0));
- 
     return rt;
 }
+
+static OPERATE_RET __camera_init(void)
+{
+    OPERATE_RET rt = OPRT_OK;
+    TDL_CAMERA_CFG_T cfg;
+
+    memset(&cfg, 0, sizeof(TDL_CAMERA_CFG_T));
+
+    sg_tdl_camera_hdl = tdl_camera_find_dev(CAMERA_NAME);
+    if(NULL == sg_tdl_camera_hdl) {
+        PR_ERR("camera dev %s not found", CAMERA_NAME);
+        return OPRT_NOT_FOUND;
+    }
+
+    cfg.fps     = EXAMPLE_CAMERA_FPS;
+    cfg.width   = EXAMPLE_CAMERA_WIDTH;
+    cfg.height  = EXAMPLE_CAMERA_HEIGHT;
+    cfg.out_fmt = TDL_CAMERA_FMT_H264;
+
+    cfg.get_encoded_frame_cb = __get_camera_h264_frame_cb;
+
+    TUYA_CALL_ERR_RETURN(tdl_camera_dev_open(sg_tdl_camera_hdl, &cfg));
+
+    PR_NOTICE("camera init success");
+
+    return OPRT_OK;
+}
+
 
 /**
  * @brief user_main
  *
+ * @param[in] param:Task parameters
  * @return none
  */
 void user_main(void)
@@ -166,28 +171,19 @@ void user_main(void)
     OPERATE_RET rt = OPRT_OK;
 
     /* basic init */
-    tal_log_init(TAL_LOG_LEVEL_DEBUG, 1024, (TAL_LOG_OUTPUT_CB)tkl_log_output);
+    tal_log_init(TAL_LOG_LEVEL_DEBUG, 4096, (TAL_LOG_OUTPUT_CB)tkl_log_output);
 
-    PR_NOTICE("Application information:");
-    PR_NOTICE("Project name:        %s", PROJECT_NAME);
-    PR_NOTICE("App version:         %s", PROJECT_VERSION);
-    PR_NOTICE("Compile time:        %s", __DATE__);
-    PR_NOTICE("TuyaOpen version:    %s", OPEN_VERSION);
-    PR_NOTICE("TuyaOpen commit-id:  %s", OPEN_COMMIT);
-    PR_NOTICE("Platform chip:       %s", PLATFORM_CHIP);
-    PR_NOTICE("Platform board:      %s", PLATFORM_BOARD);
-    PR_NOTICE("Platform commit-id:  %s", PLATFORM_COMMIT);
+    /*hardware register*/
+    board_register_hardware();
 
-    TUYA_CALL_ERR_LOG(__fs_sd_init());
+    TUYA_CALL_ERR_LOG(__sdcard_init());
 
-    TUYA_CALL_ERR_LOG(__example_dvp_init());
+    TUYA_CALL_ERR_LOG(__camera_init());
 
     while(1) {
-        tal_system_sleep(10*1000);
-        PR_NOTICE("dvp is running, captured frame path: %s", captured_frame_path);
+        tal_system_sleep(1000);
     }
 
-    return;
 }
 
 /**
@@ -219,6 +215,8 @@ static THREAD_HANDLE ty_app_thread = NULL;
  */
 static void tuya_app_thread(void *arg)
 {
+    (void) arg;
+
     user_main();
 
     tal_thread_delete(ty_app_thread);
@@ -227,7 +225,7 @@ static void tuya_app_thread(void *arg)
 
 void tuya_app_main(void)
 {
-    THREAD_CFG_T thrd_param = {4096, 4, "tuya_app_main"};
+    THREAD_CFG_T thrd_param = {1024 * 4, 4, "tuya_app_main"};
     tal_thread_create_and_start(&ty_app_thread, NULL, NULL, tuya_app_thread, NULL, &thrd_param);
 }
 #endif
