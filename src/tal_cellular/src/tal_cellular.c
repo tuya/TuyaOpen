cee839520055cac373170921884744710e93a87d
YangJie <shiliu.yang@tuya.com>
2025-11-25 17:49:19 +0800

    feat: Optimize the initialization and status management of the cellular module

--- src/tal_cellular/src/tal_cellular.c
+++ src/tal_cellular/src/tal_cellular.c
@@ -14,6 +14,7 @@
 
 #if defined(ENABLE_CELLULAR) && (ENABLE_CELLULAR == 1)
 
+#include "tkl_init_cellular.h"
 #include "tkl_cellular.h"
 
 /***********************************************************
@@ -31,6 +32,7 @@
 /***********************************************************
 ***********************variable define**********************
 ***********************************************************/
+static TKL_CELLULAR_DESC_T *sg_cellular = NULL;
 
 /***********************************************************
 ***********************function define**********************
@@ -38,27 +40,51 @@
 
 OPERATE_RET tal_cellular_init(TAL_CELLULAR_BASE_CFG_T *cfg)
 {
-    return tkl_cellular_init((TKL_CELLULAR_BASE_CFG_T *)cfg);
+    if (sg_cellular == NULL) {
+        sg_cellular = tkl_cellular_desc_get();
+    }
+
+    if (sg_cellular == NULL || sg_cellular->init == NULL) {
+        return OPRT_NOT_SUPPORTED;
+    }
+
+    return sg_cellular->init((TKL_CELLULAR_BASE_CFG_T *)cfg);
 }
 
 OPERATE_RET tal_cellular_get_status(TAL_CELLULAR_STAT_E *stat)
 {
-    return tkl_cellular_get_status((TKL_CELLULAR_STAT_E *)stat);
+    if (sg_cellular == NULL || sg_cellular->get_status == NULL) {
+        return OPRT_NOT_SUPPORTED;
+    }
+
+    return sg_cellular->get_status((TKL_CELLULAR_STAT_E *)stat);
 }
 
 OPERATE_RET tal_cellular_set_status_cb(TAL_CELLULAR_STATUS_CHANGE_CB cb)
 {
-    return tkl_cellular_set_status_cb((TKL_CELLULAR_STATUS_CHANGE_CB)cb);
+    if (sg_cellular == NULL || sg_cellular->set_status_cb == NULL) {
+        return OPRT_NOT_SUPPORTED;
+    }
+
+    return sg_cellular->set_status_cb((TKL_CELLULAR_STATUS_CHANGE_CB)cb);
 }
 
 OPERATE_RET tal_cellular_get_ip(NW_IP_S *ip)
 {
-    return tkl_cellular_get_ip((NW_IP_S *)ip);
+    if (sg_cellular == NULL || sg_cellular->get_ip == NULL) {
+        return OPRT_NOT_SUPPORTED;
+    }
+
+    return sg_cellular->get_ip((NW_IP_S *)ip);
 }
 
 OPERATE_RET tal_cellular_get_ipv6(NW_IP_TYPE type, NW_IP_S *ip)
 {
-    return tkl_cellular_get_ipv6(type, (NW_IP_S *)ip);
+    if (sg_cellular == NULL || sg_cellular->get_ipv6 == NULL) {
+        return OPRT_NOT_SUPPORTED;
+    }
+
+    return sg_cellular->get_ipv6((NW_IP_TYPE)type, (NW_IP_S *)ip);
 }
 
 #endif // defined(ENABLE_CELLULAR) && (ENABLE_CELLULAR == 1)
